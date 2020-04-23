40f61692b6d66171da1cb6df03c4898c8a453d59
Utushka <deltacentr24@gmai.com>
2020-04-23 20:34:57 +0300

    бред

--- Unit_API_VK.py
+++ Unit_API_VK.py
@@ -1,6 +1,21 @@
 import requests
 
-class Global_friends:
+
+class User:
+    def __init__(self, id):
+        self.id = id
+        print(f'https://vk.com/id{id}')
+
+
+    def __str__(self):
+        return f'пользователь {self.id}'
+
+    def __and__(self, other):
+        self.total_id = [str(self.id), str(other.id)]
+
+
+
+class Global_friends(User):
     def __init__(self):
         self.token = 'f521c678206906cf2f8a5d179f463407449a616469bddb48c7ebd1eab8b99a45b47c242adf63cef2b6c77'
 
@@ -23,6 +38,18 @@ class Global_friends:
         a = 'https://vk.com/id'
         for user in self.data['response']:
             print('{}{}\n'.format(a, user))
+
+user1 = User(115848491)
+print(user1.id)
+user2 = User(99982799)
+print(user2.id)
+
+user1.__and__(user2)
+print(user1.total_id)
+
+
+
+
 
 total = Global_friends()
 total.friend(115848491, 99982799)
